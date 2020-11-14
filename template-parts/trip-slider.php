4359aff8805b158e820edc2bab80e95832cd8459
unknown <bbassett@kent.edu>
2020-11-14 15:32:56 -0500

    trip slider actual integration

--- template-parts/trip-slider.php
+++ template-parts/trip-slider.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying multi article duo grid
+ * Trip slider
  *
  * @package igl2020
  */
@@ -8,28 +8,18 @@
 if ( ! defined( 'ABSPATH' ) ){
 	exit; // Exit if accessed directly
 } 
-$myposts = get_posts( array(
-    'posts_per_page' => 3,
-    'category'       => 3,
-    'paged' => 2
-) );
+$myposts = get_some_posts('Trip Quotes');
 
 ?>
 <section class="trip-sliders block relative <?php echo $slideid ?>">
 <div class="">
 <div class="tripslider-<?php echo $slideid ?>  ">
+<?php foreach($myposts as $key => $blurb){ ?>
     <div class="slide <?php echo $slideid ?> 1  bg-black gap-5 grid pb-10 italic pl-5 pr-5 pt-10 slide sm:pl-40 sm:pr-40 text-2xl text-white">
-        <p class="quote">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p> 
-        <p class="quotee p-5 pr-0 flex justify-end">Teacher Training Trips</p>
+        <p class="quote"><?php echo trim($blurb['excerpt']); ?></p> 
+        <p class="quotee p-5 pr-0 flex justify-end"><?php echo trim($blurb['subtext']); ?></p>
     </div>
-    <div class="slide <?php echo $slideid ?> 2  bg-black gap-5 grid pb-10 italic pl-5 pr-5 pt-10 slide sm:pl-40 sm:pr-40 text-2xl text-white">
-        <p class="quote">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p> 
-        <p class="quotee  p-5 pr-0 flex justify-end">Teacher Training Trips</p>
-    </div>
-    <div class="slide <?php echo $slideid ?> 3 1 bg-black gap-5 grid pb-10 italic pl-5 pr-5 pt-10 slide sm:pl-40 sm:pr-40 text-2xl text-white">
-        <p class="quote">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p> 
-        <p class="quotee p-5 pr-0 flex justify-end">Teacher Training Trips</p>
-    </div>
+<?php } ?>
 
 </div>
 </div>
