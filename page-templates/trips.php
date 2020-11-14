339db17e50e48349651cfb7d51909b452e07ac0e
unknown <bbassett@kent.edu>
2020-11-14 15:41:22 -0500

    adsf

--- page-templates/trips.php
+++ page-templates/trips.php
@@ -16,6 +16,7 @@ if ( ! defined( 'ABSPATH' ) ){
 <?php include( locate_template( 'template-parts/trip-slider.php', false, false) ); ?>
 <?php $slideid=2;	get_template_part( 'template-parts/trip-come-and-see'); ?>
 <?php  include( locate_template( 'template-parts/trip-slider.php', false, false) ); ?>
+<?php the_content() ?> 
 <!-- trip form -->
 <?php	get_template_part( 'template-parts/featured-fund'); ?>
 
