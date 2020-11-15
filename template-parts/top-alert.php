1ca59a1132c52da7257fb939eeaaffb7f603a58a
unknown <bbassett@kent.edu>
2020-11-15 08:52:42 -0500

    asdf

--- template-parts/top-alert.php
+++ template-parts/top-alert.php
@@ -16,9 +16,9 @@ if(!empty($alert)){
 ?>
 <section class="alert">
 	<div class="igl-background-primary ">
-		<div class="container mx-auto py-4 px-5 flex flex-wrap flex-col">
+		<div class="container mx-auto py-4 px-5 ">
 			<div class="flex md:flex-no-wrap flex-wrap justify-center ">
-                <p class="-m-1 flex signuptext sm:h-1 text-2xl text-center text-md text-white"><span class="font-primary text-2xl"><?php echo $alertname ?>:</span> 
+                <p class=" text-center text-md text-white"><span class="font-primary text-2xl"><?php echo $alertname ?>:</span> 
                 <?php echo $alert ?> For more info, contact <?php echo $alertcontact ?> at <a  class="bold" href="<?php echo $alertemail ?>"><?php echo $alertemail?></a>
             </p>
 			</div>
