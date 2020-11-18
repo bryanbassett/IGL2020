d74beac145901744cf67d564738c2d8a30ff23d5
unknown <bbassett@kent.edu>
2020-11-18 06:21:28 -0500

    adsf

--- template-parts/hero-campaign.php
+++ template-parts/hero-campaign.php
@@ -16,7 +16,7 @@ $camphero = $camphero[0];
 <section class="flex flex-col flex-wrap inline-block m-auto ml-auto mr-auto p-0 bg-cover bg-center  w-full h-full text-center relative campaignhero" style="background-image:url('<?php echo get_the_post_thumbnail_url( ); ?>')">
 
     <div class="grid grid-cols-1 lg:grid-cols-2 campgrid">
-        <div class="bg-opacity-75 bg-white camp-left lg:p-20 p-1 text-center md:h-full md:pt-32">
+        <div class="bg-opacity-75 bg-white camp-left lg:flex lg:min-h-screen lg:pb-auto lg:pt-auto md:h-full md:pt-32 p-1 text-center">
             <div class="h-full">
                 <div class="flex flex-col h-full mb-auto mt-auto">
                     <div class="campsidetext lg:mb-10 lg:mt-10 lg:text-5xl m-auto md:pb-auto md:pt-auto md:text-6xl pt-16 sm:pt-0 sm:text-4xl text-4xl text-5xl text-white uppercase w-11/12">  <?php echo str_replace('<p></p>','',preg_replace(array('<p>','</p>',),array('',''),$camphero['excerpt'],1)); ?></div>
