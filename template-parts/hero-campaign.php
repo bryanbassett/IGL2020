b8a2962cc2748ab3e38e397adeb1d0e809c799d2
unknown <bbassett@kent.edu>
2020-11-18 06:11:05 -0500

    adsf

--- template-parts/hero-campaign.php
+++ template-parts/hero-campaign.php
@@ -19,9 +19,9 @@ $camphero = $camphero[0];
         <div class="bg-opacity-75 bg-white camp-left lg:p-20 p-1 text-center">
             <div class="h-full">
                 <div class="flex flex-col h-full mb-auto mt-auto">
-                    <div class="campsidetext lg:mb-10 lg:mt-10 lg:text-5xl m-auto text-4xl text-white uppercase w-11/12">  <?php echo str_replace('<p></p>','',preg_replace(array('<p>','</p>',),array('',''),$camphero['excerpt'],1)); ?></div>
+                    <div class="campsidetext lg:mb-10 lg:mt-10 lg:text-5xl m-auto md:pb-auto md:pt-auto md:text-6xl pt-16 sm:pt-0 sm:text-4xl text-4xl text-5xl text-white uppercase w-11/12">  <?php echo str_replace('<p></p>','',preg_replace(array('<p>','</p>',),array('',''),$camphero['excerpt'],1)); ?></div>
 
-                    <div class="space-x-2 mb-3 lg:mb-0">
+                    <div class="lg:mb-0 mb-3 md:pb-40 space-x-2">
                     <a href="<?php echo $camphero['speciallink']; ?>" class="py-1 px-8 rounded-lg shadow-md hover:shadow-xl igl-button igl-red-button transition duration-300 mt-2 mb-2 inline-block uppercase"><?php echo $camphero['subtext']; ?></a>
                     </div>
 
@@ -29,7 +29,7 @@ $camphero = $camphero[0];
                 </div>
                 
             </div>
-        <div class="camp-right">
+        <div class="camp-right hidden lg:block">
         </div>
     </div>
  
