fa7dd4579465d47cd4d579ac5f08a1b1897234c7
unknown <bbassett@kent.edu>
2020-11-19 05:58:56 -0500

    adsf

--- template-parts/news-bar.php
+++ template-parts/news-bar.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ){
 <section class="newsbar">
 	<div class="igl-background-secondary font-primary">
 		<div class="mx-auto py-4 px-5 flex flex-wrap flex-col">
-			<div class="flex md:flex-no-wrap flex-wrap justify-center ">
+			<div class="flex  flex-wrap justify-center ">
 				<p class="flex font-bold font-primary h-full mb-auto mt-auto signuptext text-2xl text-center text-md text-white">SIGN UP TO SERVE WITH FIELD & PRAYER UPDATES: </p>
 				<?php echo do_shortcode('[gravityform id="3" title="false" description="false" ajax="true" tabindex="1"]'); ?>
 			</div>
