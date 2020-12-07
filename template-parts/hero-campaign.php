13160f358ef7fc5d4410db9d1bdf731460e46e1d
unknown <bbassett@kent.edu>
2020-12-07 14:11:49 -0500

    azsdf

--- template-parts/hero-campaign.php
+++ template-parts/hero-campaign.php
@@ -14,7 +14,28 @@ $camphero = get_some_posts('Campaign Hero Blurb',false,0,$post->post_name);
 $camphero = $camphero[0];
 ?>
 <div class="hidden">
+<?php $args = array(
+  'tag' => $post->post_name, 
+  'posts_per_page' => 1); ?>
+
+  <?php $posts = new WP_Query( $args ); ?>
+  <?php if ($recent_posts->have_posts()) : 
+        while ($recent_posts->have_posts()) : $recent_posts->the_post();
+   ?>
+
+
+    <li><?php the_title(); ?></li>
+
+  <?php endwhile; 
+
+     else :
+     echo "Nothing";
+      endif;
+      wp_reset_postdata();
+  ?>
     <?php 
+
+    
     $d = get_posts(array('tag' => $post_slug));
    // $d= get_some_posts('Campaign Hero Blurb',false,0,$post->post_name); 
     print_r($d[0]);
